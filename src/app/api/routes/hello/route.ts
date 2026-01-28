82bd5875bdd6d48e131024137492692ad1bb25b7
Enkhee-iu <enkheetuyatsetseg@gmail.com>
2026-01-28 18:27:35 +0800

    ci deployment

--- src/app/api/routes/hello/route.ts
+++ src/app/api/routes/hello/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   return NextResponse.json({
     method: "GET",
-    message: "Hello Worldsss",
+    message: "Hello Worldssssssssssss",
   });
 }
 
