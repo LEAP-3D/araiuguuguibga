38065a53fd55f2d5de280a42906f6fb0130b04b4
25LP5135 25LP5135 <25LP5135@G301-CC-206.local>
2026-01-29 10:29:34 +0800

    header saijruulalt

--- src/app/_components/LanguageSwitcher.tsx
+++ src/app/_components/LanguageSwitcher.tsx
@@ -1,10 +1,10 @@
 export default function LanguageSwitcher() {
   return (
-    <div className="flex items-center bg-gray-100 rounded-full p-0.5 shadow-sm">
+    <div className="flex items-center bg-orange-50 rounded-full p-0.5 shadow-sm">
       <button className="px-2 py-0.5 text-xs font-medium rounded-full bg-[#f06e42] text-white shadow-sm">
         EN
       </button>
-      <button className="px-2 py-0.5 text-xs font-medium rounded-full text-[#f06e42] hover:bg-gray-50 transition-colors">
+      <button className="px-2 py-0.5 text-xs font-medium rounded-full text-black hover:bg-gray-50 transition-colors ">
         MGL
       </button>
     </div>
