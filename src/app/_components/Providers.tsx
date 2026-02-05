4dbf8640f47f6217eb4017923ba2f3fc221ea704
Enkhee-iu <enkheetuyatsetseg@gmail.com>
2026-02-05 19:51:09 +0800

    zassan huwilbar

--- src/app/_components/Providers.tsx
+++ src/app/_components/Providers.tsx
@@ -4,11 +4,13 @@ import { PostsProvider } from "@/lib/postsContext";
 import { ClinicsProvider } from "@/lib/clinicsContext";
 import { SyncUserToDb } from "./SyncUserToDb";
 
+const hasClerkKey = typeof process !== "undefined" && Boolean(process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
+
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <PostsProvider>
       <ClinicsProvider>
-        <SyncUserToDb />
+        {hasClerkKey ? <SyncUserToDb /> : null}
         {children}
       </ClinicsProvider>
     </PostsProvider>
