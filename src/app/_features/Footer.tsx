9da08336bdbc2960f6691c5d412fe4650cf750dd
Ujin Munkhjargal <Ujin@Gandis-MacBook-Pro-2.local>
2026-02-28 05:21:57 +0800

    footer mongol

--- src/app/_features/Footer.tsx
+++ src/app/_features/Footer.tsx
@@ -4,18 +4,18 @@ export default function Footer() {
   return (
     <div className="bg-[#fdc4a148] w-[] h-60 flex items-center justify-center text-black" style={{ fontFamily: 'system-ui, -apple-system, BlinkMacSystemFont, sans-serif' }}>
       <div className="flex flex-col gap-2 text-center items-center">
-        <p className="text-[30px] font-black">Help Bring Them Home</p>
-        <p className="tex-[20px] w-110 text-gray-800 text-center mb-2">Every report counts. Whether you found a stray or lost your beloved pet, our community is here to help.</p>
+        <p className="text-[30px] font-black"> Гэртэй болгоё</p>
+        <p className="tex-[20px] w-110 text-gray-800 text-center mb-2">Та амьтан олсон эсвэл хайртай амьтнаа алдсан бол манай вебсайт танд туслахад бэлэн.</p>
         <div className="flex gap-4">
           <div className="flex items-center gap-2">
             <Link href="/dashboard/add-post">
               <div className="py-2 px-8 bg-[#f24e515a] rounded-2xl cursor-pointer">
-                <span className="text-white">Post an Animal</span>
+                <span className="text-white font-medium">Амьтан постлох</span>
               </div>
             </Link>
             <Link href="/dashboard/map">
-              <div className="py-2 px-10 border-2 border-[#f24e515a] rounded-2xl cursor-pointer">
-                <span className="text-black ">Explore map</span>
+              <div className="py-2 px-6 border-2 border-[#f24e515a] rounded-2xl cursor-pointer">
+                <span className="text-black  font-medium">Газрын зураг харах</span>
               </div>
             </Link>
           </div>
